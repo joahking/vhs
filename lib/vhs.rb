d516a86c55c097fd743eb1ff09c9b76182583712
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2014-12-10 15:59:58 +0100

    Cassette name is infered from the request path

--- lib/vhs.rb
+++ lib/vhs.rb
@@ -15,10 +15,27 @@ module VHS
     require "vhs/typhoeus_stub"
   end
 
+  # The cassette name is made out of the request path.
+  # Example:
+  # for a request with path "/rest/users/users/1"
+  # the cassette name will be "/rest/users/users/user_id"
   def cassette_name(request)
-    #TODO
-    # extract cassette name from request
-    "rest/users/users/user_id"
+    uri = URI request.url
+    #TODO use uri.host in name to use other apis
+    elems = uri.path.split "/"
+    new_elems = []
+    elems.each_index do |idx|
+      new_elems[idx] = if idx > 0 && is_integer?(elems[idx])
+                         "#{ elems[idx -1].singularize }_id"
+                       else
+                         elems[idx]
+                       end
+    end
+    new_elems.join "/"
+  end
+
+  def is_integer?(str)
+    str.to_i.to_s == str
   end
 end
 
