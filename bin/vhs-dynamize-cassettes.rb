0663fe7eec6e9c439a95c59c308f179c4943c3b4
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2015-02-06 11:33:19 +0100

    Script vhs-dynamize-cassettes.rb prints some feedback

--- bin/vhs-dynamize-cassettes.rb
+++ bin/vhs-dynamize-cassettes.rb
@@ -4,4 +4,5 @@ bin_dir = File.expand_path(File.dirname(__FILE__))
 shell_script_path = File.join(bin_dir, 'dynamize_cassettes.sh')
 
 `#{shell_script_path}`
+puts 'VCR cassettes have being dynamized'
 
