ca12e1e0f2d95ca7720282df38cc2e0b0c782cf7
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2015-02-27 16:26:44 +0100

    CLI command: config cli done. Cleanup using Thor::Actions

--- lib/vhs/cli/config.rb
+++ lib/vhs/cli/config.rb
@@ -1,17 +1,25 @@
 module VHS
   class Config < Thor
+    include Thor::Actions
+
+    desc 'cli', 'Configures the VHS CLI to run advanced commands'
+    def cli
+      copy_file 'cli.yml', '.vhs.yml'
+      puts "CLI config file .vhs.yml copied to ."
+    end
 
     desc 'rspec DESTINATION_PATH', 'Configures rspec to use VHS'
-    method_option :destination_path, default: 'spec/support'
+    method_option :destination_path, default: 'spec/support/vhs.rb'
     def rspec
       destination_path = options[:destination_path]
-
-      dir = File.expand_path(File.dirname(__FILE__))
-      rspec_config_file = File.open File.join(dir, '../../../lib/generators/rspec/vhs.rb')
-
-      FileUtils.cp rspec_config_file, destination_path
-
+      copy_file 'rspec/vhs.rb', destination_path
       puts "Spec config file vhs.rb copied to #{ destination_path }"
+    end
+
+private
+
+    def self.source_root
+      File.join File.dirname(__FILE__), '../../generators/'
     end
 
   end
