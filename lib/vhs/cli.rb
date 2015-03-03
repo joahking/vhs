fa8077646504249ccd3a89b04c5825ce69ff33f5
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2015-03-03 11:01:55 +0100

    CLI command list done

--- lib/vhs/cli.rb
+++ lib/vhs/cli.rb
@@ -11,6 +11,23 @@ module VHS
 
     desc 'cassettes SUBCOMMAND ARGS', 'Cassettes handling commands'
     subcommand 'cassettes', Cassettes
+
+    desc 'list CODE', 'List your cassettes. CODE values are: all (default), error, or a regexp to match an HTTP status code, e.g. 200, 404, 503, 0, 2.., 5.., etc'
+    def list(code = 'all')
+      path = VHS::CLILoader.new.cassettes_path
+
+      case code
+      when 'all'
+        puts 'Cassettes: all'
+        puts `find #{ path } -name "*.yml"`
+      when 'error'
+        puts 'Cassettes: error'
+        puts `grep 'code: [^200]' #{ path } -Rn | sed 's/\.yml.*/.yml/g'`
+      else
+        puts "Cassettes: code #{ code }"
+        puts `grep 'code: #{ code }' #{ path } -Rn | sed 's/\.yml.*/.yml/g'`
+      end
+    end
   end
 
 end
--- lib/vhs/cli/cli_loader.rb
+++ lib/vhs/cli/cli_loader.rb
@@ -1,6 +1,6 @@
 module VHS
   class CLILoader
-
+    #TODO turn this one into VHS::Loader and use it in spec/support/vhs.rb
     # Public: configures VHS from .vhs.yml config file and loads it.
     def load_vhs
       configure_vcr
@@ -8,11 +8,23 @@ module VHS
       VHS.load
     end
 
+    #TODO move it to config class?
+    def cassettes_path
+      config['vcr']['cassette_library_dir']
+    end
+
   private
+
+    def config
+      @config ||= begin
+                    pwd = "#{ `pwd` }".gsub(/\n/, '')
+                    YAML.load_file "#{ pwd }/.vhs.yml"
+                  end
+    end
 
     def configure_vcr
       VCR.configure do |vcr|
-        cli_config['vcr'].each do |key, value|
+        config['vcr'].each do |key, value|
           if vcr.respond_to? "#{ key }="
             vcr.send "#{ key }=", value
           else
@@ -24,17 +36,10 @@ module VHS
 
     def configure_vhs
       VHS.configure do |vhs|
-        cli_config['vhs'].each do |key, value|
+        config['vhs'].each do |key, value|
           vhs.send "#{ key }=", value
         end
       end
-    end
-
-    def cli_config
-      @cli_config ||= begin
-                        pwd = "#{ `pwd` }".gsub(/\n/, '')
-                        YAML.load_file "#{ pwd }/.vhs.yml"
-                      end
     end
 
   end
