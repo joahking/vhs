ca75de526640133af1d12e324aa6d6e7a388b708
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2015-03-03 16:35:49 +0100

    CLI cassette command dynamize moved to CLI class and some other cleanups

--- lib/vhs/cli.rb
+++ lib/vhs/cli.rb
@@ -1,5 +1,4 @@
 require 'thor'
-require 'vhs/cli/cassettes'
 require 'vhs/cli/config'
 require 'vhs/cli/cli_loader'
 
@@ -7,75 +6,86 @@ module VHS
 
   #TODO add aliases
   class CLI < Thor
+    include Thor::Actions
+
     desc 'config SUBCOMMAND ARGS', 'Configures VHS for test frameworks'
     subcommand 'config', Config
 
-    desc 'cassettes SUBCOMMAND ARGS', 'Cassettes handling commands'
-    subcommand 'cassettes', Cassettes
-
     desc 'list CASSETTE', 'List your cassettes. CASSETTE values are: all (default), error, a regexp to match an HTTP status code (must start with a digit)'
     def list(cassette = 'all')
-      path = VHS::CLILoader.new.cassettes_path
-
       case cassette
       when 'all'
         puts 'Listing all cassettes'
-        puts `#{ all_cassettes_cmd(path) }`
+        puts `#{ all_cassettes_cmd }`
       when 'error'
         puts 'Listing error cassettes'
-        puts `#{ error_cassettes_cmd(path) }`
+        puts `#{ error_cassettes_cmd }`
       else
         puts "Cassettes by regexp #{ cassette }"
-        puts `#{ all_cassettes_cmd(path, cassette) }`
+        puts `#{ all_cassettes_cmd(cassette) }`
       end
     end
 
     desc 'update CASSETTE', 'Updates cassettes. CASSETTE values are: all (default), a filepath to a cassette, error, a regexp to match an HTTP status code (must start with a digit)'
     def update(cassette = 'all')
-      loader = VHS::CLILoader.new
-      path = loader.cassettes_path
-
       cassettes = case cassette
                   when 'all'
                     puts 'Updating all cassettes'
-                    `#{ all_cassettes_cmd(path) } | #{ clean_output_cmd }`
+                    `#{ all_cassettes_cmd } | #{ rm_trailing_chars_cmd }`
                   when 'error'
                     puts 'Updating error cassettes'
-                    `#{ error_cassettes_cmd(path) } | #{ clean_output_cmd }`
+                    `#{ error_cassettes_cmd } | #{ rm_trailing_chars_cmd }`
                   when /\A\d.?.?/
                     puts "Updating cassettes by regexp #{ cassette }"
-                    `#{ all_cassettes_cmd(path, cassette) } | #{ clean_output_cmd }`
+                    `#{ all_cassettes_cmd(cassette) } | #{ rm_trailing_chars_cmd }`
                   else
                     cassette # it is a filename
                   end
 
-      loader.load_vhs
+      VHS::CLILoader.new.load_vhs
       cassettes.split(/\n/).each do |cassette|
         puts "Updating cassette #{ cassette }"
         VHS.cassette_update cassette
       end
     end
 
+    #TODO move to XING dynamizers
+    desc 'dynamize', 'Replaces api urls in cassettes with <%= api_host %>'
+    def dynamize
+      cassettes = `#{ all_cassettes_cmd } | #{ rm_trailing_chars_cmd }`
+
+      cassettes.split(/\n/).each do |cassette|
+        gsub_file cassette, /uri: http:\/\/.*.env.xing.com:3007\/rest/, 'uri: <%= api_host %>'
+      end
+      puts 'Cassettes have being dynamized'
+    end
+
     private
 
+    def self.source_root
+      @loader ||= VHS::CLILoader.new.cassettes_path
+    end
+
     # Private: finds all cassettes.
-    # - path the path to the cassettes
-    # - code a regexp for the HTTP status code, if not passed all cassettes are found
+    # - code: regexp for the HTTP status code, if not passed all cassettes are found
     # Returns a list of cassettes with code behind, \n separated.
-    def all_cassettes_cmd(path, code = '')
-      "grep 'code: #{ code }' #{ path } -R"
+    def all_cassettes_cmd(code = '')
+      "grep 'code: #{ code }' #{ self.class.source_root } -R | #{ rm_trailing_colon_cmd }"
     end
 
     # Private: finds all cassettes which code is not 200.
-    # - path the path to the cassettes
     # Returns a list of cassettes with code behind, \n separated.
-    def error_cassettes_cmd(path)
-      "grep 'code:' #{ path } -R | grep -v 'code: 200'"
+    def error_cassettes_cmd
+      "grep 'code:' #{ self.class.source_root } -R | grep -v 'code: 200' | #{ rm_trailing_colon_cmd }"
     end
 
     # Private: removes everything from output leaving just filenames.
-    def clean_output_cmd
+    def rm_trailing_chars_cmd
       "sed 's/\.yml.*/.yml/g'"
+    end
+
+    def rm_trailing_colon_cmd
+      "sed 's/\.yml:/.yml/g'"
     end
 
   end
